24c29a0b0953f3fb657409ff0416859269d93041
LarryWatterson <49096147+LarryWatterson@users.noreply.github.com>
2025-01-25 00:13:56 +0300

    added missed structs and enums

--- 1.SVN/Client/Packet.h
+++ 1.SVN/Client/Packet.h
@@ -7,3 +7,40 @@
 #ifdef BOSS_DAMAGE_RANKING_PLUGIN
     HEADER_GC_BOSS_DMG_RANKING = 241,
 #endif
+
+// add anywhere
+
+#ifdef BOSS_DAMAGE_RANKING_PLUGIN
+enum class BadAffectType : uint8_t {
+    NONE = 0 << 0,
+    POISON = 1 << 0,
+    FIRE = 1 << 1,
+#ifdef ENABLE_WOLFMAN_CHARACTER
+    BLEED = 1 << 2,
+#endif
+};
+
+enum class EPacketCGBossDamageRankingSubHeaderType : uint8_t {
+    BOSS_DMG_RANKING_RANK_INFO,
+    BOSS_DMG_RANKING_DAMAGE_INFO,
+};
+
+struct SPacketCGBossDamageRanking
+{
+    SPacketCGBossDamageRanking() : header(HEADER_GC_BOSS_DMG_RANKING) {}
+    uint8_t header;
+};
+
+struct SPacketGCRankingGeneralInfo
+{
+    uint8_t rank_size;
+};
+
+struct SPacketGCBossDamageRankingInfo
+{
+    uint8_t race;
+    char name[CHARACTER_NAME_MAX_LEN + 1];
+    uint8_t percent_damage;
+    uint8_t bad_affect_flag;
+};
+#endif
